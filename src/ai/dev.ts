3b669470e63e24484c9280b8b143f7087f681bd3
Sruthi Kranthi <sruthikranthi24@gmail.com>
2025-11-26 05:12:28 +0000

    ok., now make the application ready for deployment

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,4 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/ai-powered-chat-safety.ts';
-import '@/ai/flows/text-to-speech.ts';
 import '@/ai/flows/generate-meditation-script.ts';
