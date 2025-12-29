21dc2841d9a32e7d681a2bb0b148cbd5afd74a38
download--17- <kranthisuryadevara25@gmail.com>
2025-12-29 08:06:46 +0530

    Update favicon to use brand image instead of Firebase logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,18 +62,21 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' }, // Default favicon
       { url: '/icon-72x72.png', sizes: '72x72', type: 'image/png' },
       { url: '/icon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/icon-128x128.png', sizes: '128x128', type: 'image/png' },
       { url: '/icon-144x144.png', sizes: '144x144', type: 'image/png' },
       { url: '/icon-152x152.png', sizes: '152x152', type: 'image/png' },
-      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-384x384.png', sizes: '384x384', type: 'image/png' },
       { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
       { url: '/icon-152x152.png', sizes: '152x152', type: 'image/png' },
       { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+    ],
+    shortcut: [
+      { url: '/icon-192x192.png', type: 'image/png' },
     ],
   },
 };
@@ -86,6 +89,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="shortcut icon" type="image/png" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
